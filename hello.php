3ccba08e71cc9574fa47d63b6add9d183f829b25
synj <matthew.gregory@hotmail.co.uk>
2018-04-26 15:46:18 +0100

    change readme.txt

--- hello.php
+++ hello.php
@@ -1,75 +1,80 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package Aurora-borealis
+ * @version 1.0
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Aurora Borealis
+Plugin URI:
+Description: A delightfully devilish wordpress plugin forked from "Hello Dolly". When activated you will randomly see a line of dialog from Seymour Skinners unforgettable luncheon in the upper right of your admin screen on every page.
+Author: Matthew Daryl Gregory
+Version: 1.0
+Author URI: http://matthewdarylgregory.com/
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+function steamed_hams_get_lyric() {
+	/** This is the dialog to steamed hams */
+	$dialog = "Well, Seymour, I made it- despite your directions.
+	Ah. Superintendent Chalmers. Welcome.
+	I hope you're prepared for an unforgettable luncheon.
+	Yeah. Oh, egads! My roast is ruined.
+	But what if I were to purchase fast food and disguise it as my own cooking?
+	Delightfully devilish, Seymour.
+	Ah- Skinner with his crazy explanations The superintendent's gonna need his medication When he hears Skinner's lame exaggerations There'll be trouble in town tonight
+	Seymour! Superintendent, I was just- uh, just stretching my calves on the windowsill.
+	Isometric exercise. Care to join me?
+	Why is there smoke coming out of your oven, Seymour?
+	Uh- Oh. That isn't smoke. It's steam. Steam from the steamed clams we're having.
+	Mmm. Steamed clams. Whew.
+	Superintendent, I hope you're ready for mouthwatering hamburgers.
+	I thought we were having steamed clams.
+	D'oh, no. I said steamed hams. That's what I call hamburgers.
+	You call hamburgers steamed hams?
+	Yes. It's a regional dialect.
+	Uh-huh. Uh, what region?
+	Uh, upstate New York.
+	Really. Well, I'm from Utica, and I've never heard anyone use the phrase 'steamed hams. '
+	Oh, not in Utica. No. It's an Albany expression.
+	I see. You know, these hamburgers are quite similar to the ones they have at Krusty Burger.
+	Oh, no. Patented Skinner burgers. Old family recipe. - For steamed hams. - Yes.
+	Yes. And you call them steamed hams despite the fact that they are obviously grilled.
+	Ye- You know, the- One thing I should- - Excuse me for one second.
+	Of course. Well, that was wonderful. A good time was had by all. I'm pooped.
+	Yes. I should be- Good Lord! What is happening in there?
+	Aurora borealis.
+	Uh- Aurora borealis at this time of year at this time of day in this part of the country localized entirely within your kitchen?
+	Yes. - May I see it? No.
+	Seymour. ! The house is on fire. !
+	No, Mother. It's just the northern lights.
+	Well, Seymour, you are an odd fellow but I must say you steam a good ham.";
 
 	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
+	$dialog = explode( "\n", $dialog );
 
 	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+	return wptexturize( $dialog[ mt_rand( 0, count( $dialog ) - 1 ) ] );
 }
 
 // This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+function aurora_borealis() {
+	$chosen = steamed_hams_get_lyric();
+	echo "<p id='seymour'>$chosen</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
+add_action( 'admin_notices', 'aurora_borealis' );
 
 // We need some CSS to position the paragraph
-function dolly_css() {
+function borealis_css() {
 	// This makes sure that the positioning is also good for right-to-left languages
 	$x = is_rtl() ? 'left' : 'right';
 
 	echo "
 	<style type='text/css'>
-	#dolly {
+	#seymour {
 		float: $x;
 		padding-$x: 15px;
-		padding-top: 5px;		
+		padding-top: 5px;
 		margin: 0;
 		font-size: 11px;
 	}
@@ -77,6 +82,6 @@ function dolly_css() {
 	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'admin_head', 'borealis_css' );
 
 ?>
